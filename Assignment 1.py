26ca34e6baef94f02d9fe087ede361b3d17010bc
Svssharsha007 <svssharsha007@gmail.com>
2025-11-17 16:44:11 +0530

    initial commit

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+### Task 1
+#  Write a Python program that does the following:
+#   1.  Takes two numbers as input from the user.
+#  2.  Performs the basic mathematical operations on these two numbers ###
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+Addition = a+b
+Subraction = a-b
+Multiplication = a*b
+Division = a/b
+Result = print("Addition :",Addition,"\n","Subration :",Subraction,"\n",
+               "Multiplication :",Multiplication,"\n","Division :",Division)
+
