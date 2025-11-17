75b6a5dffdac5c7c8f0e051b92714efd9b691d6f
Svssharsha007 <svssharsha007@gmail.com>
2025-11-17 16:57:57 +0530

    initial commit

---
+++ Assignment 1 Task 2.py
@@ -0,0 +1,9 @@
+# Task 2 #
+# Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+a = input("Enter your first name :")
+b = input("Enter your last name :")
+Result = print("Hello ,",a+" "+b,"! Welcome to the python program.")
